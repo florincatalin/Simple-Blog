1f6f9b2ebc6735c98004e6cd76b8b2a7566ad796
Florin Catalin <23093563+florincatalin@users.noreply.github.com>
2019-11-04 20:46:24 +0200

    Update HookHead.php

--- HookHead.php
+++ HookHead.php
@@ -1,7 +1,8 @@
 <?php
 defined('is_running') or die('Not an entry point...');
 
-global $addonRelativeData, $page;
+global $addonRelativeData, $addonRelativeCode, $page;
+$page->head_js[] = $addonRelativeCode . '/static/jquery.details.min.js';
 
 $url = common::GetUrl('Special_Blog_Feed');
 $page->head .= '<link rel="alternate" type="application/atom+xml" href="'.$url.'" />';
